139124c69f4011dc32c4223b08af3da0f68943ee
RIFAT1287 <92775750+RIFAT1287@users.noreply.github.com>
2024-01-27 21:01:36 +0600

    Update gogo.py

--- utils/gogo.py
+++ utils/gogo.py
@@ -156,33 +156,34 @@ class GoGoApi:
         return len(li), eps
 
     async def episode(self, id, lang):
-        global Gcookie
-        if "cookie" not in Gcookie:
-            Gcookie = {
-                "time": time.time(),
-                "cookie": self.get_gogo_cookie(
-                    "qwertytechz123@gmail.com", "P@8eqB7@YpJz5ea"
-                ),
-            }
+    global Gcookie
+    if "cookie" not in Gcookie:
+        Gcookie = {
+            "time": time.time(),
+            "cookie": self.get_gogo_cookie(
+                "qwertytechz123@gmail.com", "P@8eqB7@YpJz5ea"
+            ),
+        }
 
-        if time.time() - Gcookie["time"] > 60 * 10:
-            Gcookie = {
-                "time": time.time(),
-                "cookie": self.get_gogo_cookie(
-                    "qwertytechz123@gmail.com", "P@8eqB7@YpJz5ea"
-                ),
-            }
-        auth_gogo = Gcookie["cookie"]
+    if time.time() - Gcookie["time"] > 60 * 10:
+        Gcookie = {
+            "time": time.time(),
+            "cookie": self.get_gogo_cookie(
+                "qwertytechz123@gmail.com", "P@8eqB7@YpJz5ea"
+            ),
+        }
+    auth_gogo = Gcookie["cookie"]
 
-        data = {}
-        data["DL"] = {}
+    data = {}
+    data["DL"] = {}
 
-        async with self.session.get(
-            f"https://{self.host}/{id}", cookies={"auth": auth_gogo}
-        ) as resp:
-            soup = bs(await resp.read(), "html.parser")
+    async with self.session.get(
+        f"https://{self.host}/{id}", cookies={"auth": auth_gogo}
+    ) as resp:
+        soup = bs(await resp.read(), "html.parser")
 
-        div = soup.find("div", "anime_muti_link")
+    div = soup.find("div", "anime_muti_link")
+    if div:
         a = div.find_all("a")
         embeds = []
 
@@ -282,6 +283,12 @@ class GoGoApi:
                 data["DUB"] = embeds
                 data["DL"]["DUB"] = dlink
             return data
+    else:
+        # Handle the case when 'div' is None
+        # You might want to log a message or handle this case accordingly
+        return data
+
+            
 
     def get_gogo_cookie(self, email, password):
         s = requests.session()
